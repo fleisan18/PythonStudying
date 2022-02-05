ef842d114209d4ca35e244966eed188cbd62f64f
Leisan <fleisan18@mail.ru>
2022-02-06 00:00:36 +0500

    solved 16 task with list compr

--- NewTasks/16 task.py
+++ NewTasks/16 task.py
@@ -1,47 +1,7 @@
-# Задать список из N элементов, заполненных числами из [-N, N]. 
-# Найти произведение элементов на указанных позициях. 
-# Позиции хранятся в файле file.txt в одной строке одно число
-import random
+# 16. Задать список из n чисел последовательности (1+1/n)**n и 
+# вывести на экран их сумму
 
-data = open('test.txt', 'w')
-data.writelines('2 \n')
-data.writelines('3 \n')
-data.writelines('5 \n')
-data.close()
-
-def Filling(n):
-    list = []
-    for i in range(n):
-        list.append(random.randint(-n,n))
-    return list
-
-def Filling2 (n):
-    return [random.randint(-n, n) for i in range(n)]
-
-def Set(file):
-    data = open(file, 'r')
-    indexes = []
-    for line in data:
-        line = line.replace('\n', '')
-        indexes.append(int(line))
-    data.close()
-    return indexes
-
-def Multiplication(indexes, list):
-    result = 1
-    for i in range(len(list)):
-        if i in indexes:
-            result*=list[i]
-    return result
-
-
-# test_list = Filling(10)
-test_list = Filling2(10)
-print(test_list)
-
-indexes = Set('test.txt')
-print(indexes)
-
-print(Multiplication(indexes, test_list))
-
-
+n = 10000
+my_list = [(1+1/n)**n for i in range(n)]
+print(my_list)
+print(sum(my_list))
