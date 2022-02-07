ceb32878ec73ea29cc4ac0ba3e8f406dc987b123
Leisan <fleisan18@mail.ru>
2022-02-07 15:28:06 +0500

    changed 2 solution

--- NewTasks/20 task.py
+++ NewTasks/20 task.py
@@ -5,7 +5,6 @@ test_number = input('Введите разыскиваемое число: ')
 
 # 1
 def SearchNumber(list, wanted_number):
-    wanted_number = wanted_number
     for i in list:
         finding = i==wanted_number
         if finding:
@@ -14,10 +13,9 @@ def SearchNumber(list, wanted_number):
 
 
 # 2
-if test_list.count(test_number) > 0:
-    print(f'{test_number} присутствует')
-else:
-    print(f'{test_number} отсутствует')
+def SearchNumber2 (list, wanted_number):
+    return list.count(wanted_number) > 0
 
 
 print(SearchNumber(test_list, test_number))
+print(SearchNumber2(test_list, test_number))
