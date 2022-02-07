00def0e23a1c716e546e6657742066187f79aaaa
Leisan <fleisan18@mail.ru>
2022-02-07 20:07:03 +0500

    changed method summuneven

--- NewTasks/22 task.py
+++ NewTasks/22 task.py
@@ -7,10 +7,9 @@ def Filling(length):
     return list
 
 def SummUneven (list):
-    for i in range(0, len(list)-1, 1):
-        removed_el = list.pop(i)
-        print(removed_el)
-    return sum(list)
+    new_list = [list[i] for i in range(0, len(list), 2)]
+    print(new_list)
+    return sum(new_list)
 
 test_list = Filling(10)
 print(test_list)
